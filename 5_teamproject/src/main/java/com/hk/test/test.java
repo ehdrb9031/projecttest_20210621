12c86bb325c3c78112da231e5e19240cfd92281b
joharry <joharry@LAPTOP-M7DULIQD>
2021-10-01 13:13:43 +0900

    ㅇㄹㅇㄹ

--- 5_teamproject/src/main/java/com/hk/test/test.java
+++ 5_teamproject/src/main/java/com/hk/test/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("������������");
 		System.out.println("HIHIHIHIHIHIHI~~");
-		System.out.println("�翭");
+		System.out.println("�翭");
+		System.out.println("재웅");  
 	}
 }
