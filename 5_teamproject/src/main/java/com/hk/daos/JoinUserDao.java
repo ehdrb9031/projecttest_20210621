d2cf716632994948b5d57fc5d9d1e1e5e219a978
owner <owner@DESKTOP-NS2JBAN>
2021-10-05 15:40:01 +0900

    BDY INSERT USER

--- 5_teamproject/src/main/java/com/hk/daos/JoinUserDao.java
+++ 5_teamproject/src/main/java/com/hk/daos/JoinUserDao.java
@@ -8,9 +8,12 @@ import org.apache.ibatis.session.SqlSession;
 import com.hk.config.SqlMapConfig;
 import com.hk.dtos.JoinUserDto;
 
+
+
 public class JoinUserDao extends SqlMapConfig {
 	private String namespace="com.hk.joinuser.";
 	
+	//로그인
 	public JoinUserDto getLogin(String id, String password) {
 		JoinUserDto dto=null;
 		SqlSession sqlSession=null;
@@ -27,5 +30,25 @@ public class JoinUserDao extends SqlMapConfig {
 			sqlSession.close();
 		}
 		return dto; 
+	}
+
+	
+	// 회원가입 
+public boolean insertUser(JoinUserDto dto) {
+		
+		int count=0;
+		SqlSession sqlSession=null;
+	
+		
+		try {
+			sqlSession=getSqlSessionFactory().openSession(true);
+			count=sqlSession.insert(namespace+"insertUser", dto);
+		} catch (Exception e) {
+			System.out.println("JDBC실패:insertUser():"+getClass());
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return count>0?true:false;
 	}
 }
