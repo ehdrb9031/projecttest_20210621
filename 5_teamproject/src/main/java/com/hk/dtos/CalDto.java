93e6074c1d1e685bff2b78f4c5631c6e9beebb9a
leedonggyu <leedonggyu@LAPTOP-JAK84AN4>
2021-10-05 14:09:38 +0900

    CalDto

--- 5_teamproject/src/main/java/com/hk/dtos/CalDto.java
+++ 5_teamproject/src/main/java/com/hk/dtos/CalDto.java
@@ -9,32 +9,40 @@ public class CalDto {
 	private String wdate;
 
 
-   public CalDto() {
+	public CalDto() {
 	    super();
      }
 
-   public CalDto(int to_seq, String id, String wdate) {
-	super();
-	this.to_seq = to_seq;
-	this.id = id;
-	this.wdate = wdate;
-   }
-  
-   public int getto_seq() {
-	return to_seq;
-   }  
-
-   public String getid() {
-	return id;
-   }
-
-   public String getwdate() {
-	return wdate;
-   }
-
-   public String toString() {
-	return "CalDto [to_seq=" + to_seq + ", id=" + id + ", wdate= " + wdate + "]";
-   }
+	public CalDto(int to_seq, String id, String wdate) {
+		super();
+		this.to_seq = to_seq;
+		this.id = id;
+		this.wdate = wdate;
+	}
+	
+	public int getTo_seq() {
+		return to_seq;
+	}
+	
+	public void setTo_seq(int to_seq) {
+		this.to_seq = to_seq;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getWdate() {
+		return wdate;
+	}
+	
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
 }
 
 
