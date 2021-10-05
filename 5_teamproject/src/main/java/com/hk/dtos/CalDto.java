b3c43d5c69ca17cab4f40a7af997a99d86c65381
leedonggyu <leedonggyu@LAPTOP-JAK84AN4>
2021-10-05 14:11:39 +0900

    CalDto 이동규

--- 5_teamproject/src/main/java/com/hk/dtos/CalDto.java
+++ 5_teamproject/src/main/java/com/hk/dtos/CalDto.java
@@ -7,42 +7,39 @@ public class CalDto {
 	private int to_seq;
 	private String id;
 	private String wdate;
-
-
 	public CalDto() {
-	    super();
-     }
-
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	public CalDto(int to_seq, String id, String wdate) {
 		super();
 		this.to_seq = to_seq;
 		this.id = id;
 		this.wdate = wdate;
 	}
-	
 	public int getTo_seq() {
 		return to_seq;
 	}
-	
 	public void setTo_seq(int to_seq) {
 		this.to_seq = to_seq;
 	}
-	
 	public String getId() {
 		return id;
 	}
-	
 	public void setId(String id) {
 		this.id = id;
 	}
-	
 	public String getWdate() {
 		return wdate;
 	}
-	
 	public void setWdate(String wdate) {
 		this.wdate = wdate;
 	}
+	@Override
+	public String toString() {
+		return "CalDto [to_seq=" + to_seq + ", id=" + id + ", wdate=" + wdate + "]";
+	}
+
 }
 
 
