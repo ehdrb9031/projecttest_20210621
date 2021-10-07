87cadef18f4a10ab42dc6b8f887d8c159b624b14
owner <owner@DESKTOP-NS2JBAN>
2021-10-07 11:02:51 +0900

    notice

--- 5_teamproject/src/main/java/com/hk/daos/NoticeDao.java
+++ 5_teamproject/src/main/java/com/hk/daos/NoticeDao.java
@@ -1,9 +1,26 @@
 package com.hk.daos;
 
+import org.apache.ibatis.session.SqlSession;
+
 import com.hk.config.SqlMapConfig;
+import com.hk.dtos.NoticeDto;
 
 public class NoticeDao extends SqlMapConfig {
 	private String namespace="com.hk.notice";
 	
-	//public boolean insertNotice()
+	public boolean insertNotice(NoticeDto dto) {
+		int count = 0;
+		SqlSession sqlSession=null;
+		
+		try {
+			sqlSession=getSqlSessionFactory().openSession(true);
+			count=sqlSession.insert(namespace+"insertNotice", dto);
+		} catch (Exception e) {
+			System.out.println("JDBC실패:insertUser():"+getClass());
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		return count>0?true:false;
+	}
 }
